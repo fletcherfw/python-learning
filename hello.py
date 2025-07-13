88330a34b59318d78920c210b7f7cb0bf905318a
Fletcher <fletcher@discovered.ai>
2025-07-13 14:58:08 -0700

    Day 4: Added loop

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-name = input("What is your name? ")
-if name == "Fletcher":
-    print("Hello, BOSS ! I'm starting my Python Journey. ") 
-else:
-    print("Hello, " + name + "! I'm starting my Python Journey. ")
-
+while True:
+    name = input("What is your name? (type 'quit' to stop) ")
+    if name == "quit":
+        break
+    if name == "Fletcher":
+        print("Hello, BOSS! I'm starting my Python Journey. ") 
+    else:
+        print("Hello, " + name + "! I'm starting my Python Journey. ")
 
 
 
