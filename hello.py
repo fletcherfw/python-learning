fc9a471476b71bc8df94aea4a80a7199bd1f8574
Fletcher <fletcher@discovered.ai>
2025-07-13 14:12:15 -0700

    Day 3 code

---
+++ hello.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+if name == "Fletcher":
+    print("Hello, BOSS ! I'm starting my Python Journey. ") 
+else:
+    print("Hello, " + name + "! I'm starting my Python Journey. ")
+
+
+
+
+
